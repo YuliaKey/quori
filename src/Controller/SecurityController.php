0c6a482eb06299bf54c33f109205afdf3fdeff15
Yuliia Kolomiiets <jk182013@gmail.com>
2023-05-15 11:51:05 +0200

    feat: Mise en place du formulaire pour la reinitialisation du mot de passe

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,11 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\ResetPassword;
 use App\Entity\User;
 use App\Form\UserType;
+use App\Repository\ResetPasswordRepository;
+use App\Repository\UserRepository;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
@@ -14,6 +19,8 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class SecurityController extends AbstractController
 {
@@ -69,4 +76,73 @@ class SecurityController extends AbstractController
 
     #[Route('/logout', name: 'logout')]
     public function logout() {}
+
+    #[Route('/reset-password-request', name: 'reset-password-request')]
+    public function resetPasswordRequest(Request $request, UserRepository $userRepository, EntityManagerInterface $em, ResetPasswordRepository $resetPasswordRepository, MailerInterface $mailer) {
+
+        $emailForm = $this->createFormBuilder()
+                            ->add('email', EmailType::class, [
+                                'constraints' => [
+                                    new NotBlank([
+                                        'message' => 'Veuillez renseigner ce champ.'
+                                    ]),
+                                    new Email([
+                                        'message' => 'Veuillez entrer un email valide'
+                                    ])
+                                ]
+                            ])
+                            ->getForm();
+
+        $emailForm->handleRequest($request);
+
+        if ($emailForm->isSubmitted() && $emailForm->isValid()){
+            $email = $emailForm->get('email')->getData();
+            $user = $userRepository->findOneBy(['email' => $email]);
+
+            if($user) {
+
+                // on s'assure qu'il n' y a pas deja une demande de reset
+                $oldResetPassword = $resetPasswordRepository->findOneBy(['user' => $user]);
+                if($oldResetPassword) {
+                    $em->remove($oldResetPassword);
+                    $em->flush();
+                }
+
+                // on fait un random bytes de 40 caracteres et on en garde que 20
+                $token = substr(str_replace(['+', '=', '/'], ',', base64_encode(random_bytes(40))), 0, 20);
+
+                $resetPassword = new ResetPassword();
+                $resetPassword->setUser($user)
+                            ->setToken($token)
+                            ->setExpiredAt(new DateTimeImmutable('+2 hours')); 
+
+                $em->persist($resetPassword);
+                $em->flush();
+
+                //on envoie l'email de reinitialisation
+                $resetEmail = new TemplatedEmail();
+                $resetEmail->to($email)
+                            ->subject('Demande de reinitialisation de mot de passe')
+                            ->htmlTemplate('@email_templates/reset-password-request.html.twig')
+                            ->context([
+                                'fullname' => $user->getFullname(),
+                                'token' => $token
+                            ]);
+                $mailer->send($resetEmail);
+
+                $this->addFlash('success', 'Un email vous a ete envoye');
+                return $this->redirectToRoute('signin');
+            } else {
+                $this->addFlash('error', "Cet email n'existe pas");
+
+            }
+        }
+
+        return $this->render('security/reset-password-request.html.twig', ['form' => $emailForm->createView()]);
+    }
+
+    #[Route('/reset-password/{token}', name: 'reset-password')]
+    public function resetPassword() {
+        
+    }
 }
